ab2188199b295a3c035302ab50745ea5826ce21d
Maryan Berhe <maryanberhe@gmail.com>
2022-06-22 17:09:03 -0500

    Generated request and swap models and updated db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_21_214837) do
+ActiveRecord::Schema.define(version: 2022_06_22_220301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 2022_06_21_214837) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "swaps", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +54,5 @@ ActiveRecord::Schema.define(version: 2022_06_21_214837) do
   end
 
   add_foreign_key "listings", "users"
+  add_foreign_key "requests", "users"
 end
