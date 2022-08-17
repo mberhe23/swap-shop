eeb73fa0cd8fb6bd383a2979ef18c7e0a7a46ba8
Maryan Berhe <maryanberhe@gmail.com>
2022-08-17 14:07:36 -0500

    Added a few things to stylesheets and pundit to gemfile

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many_attached :photo
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
 end
