2196b18d242e1a42a4560cc8547fdbe2c55d3a0c
Brianna Foley <brianna.foley87@gmail.com>
2022-08-18 18:50:53 -0500

    listing card partial, user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Users
+puts "---creating fake users---"
+default_password = "123456"
+
+user_names = ["Andy", "Brianna", "Francine", "Georgia", "Jerry", "Maryan"]
+
+users = []
+user_names.each do |user|
+  users << User.create!(
+    email: "#{user.downcase}@users.com",
+    password: default_password
+  )
+end
+
+# users.map do |user|
+#   file = File.open("db/support/#{user.username}.jpg")
+#   user.photo.attach(io: file, filename: "#{user.username}.jpg", content_type: 'image/jpg')
+#   user.save!
+# end
+puts "---done---"
+
+# Listings
