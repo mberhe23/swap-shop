e59b4644c339b73882b874c34f85691ab01d2527
Brianna Foley <brianna.foley87@gmail.com>
2022-08-23 13:40:59 -0500

    added links to listings index and new swap

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  has_many_attached :photo
+  has_many_attached :photos
   validates :title, presence: true
   validates :description, presence: true
   validates :category, presence: true
