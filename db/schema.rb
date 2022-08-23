f631f87ad92684df235e72c217281d5d9889b8b8
Brianna Foley <brianna.foley87@gmail.com>
2022-08-23 16:42:41 -0500

    added ids for listing and request to swaps table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_204134) do
+ActiveRecord::Schema.define(version: 2022_08_23_205358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,10 @@ ActiveRecord::Schema.define(version: 2022_08_23_204134) do
   create_table "swaps", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "request_id"
+    t.bigint "listing_id"
+    t.index ["listing_id"], name: "index_swaps_on_listing_id"
+    t.index ["request_id"], name: "index_swaps_on_request_id"
   end
 
   create_table "users", force: :cascade do |t|
