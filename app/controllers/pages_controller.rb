968c4409f758e48d95989e975b68d7a3f6c0cde9
Brianna Foley <brianna.foley87@gmail.com>
2022-08-24 16:54:19 -0500

    requests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,11 @@ class PagesController < ApplicationController
   end
 
   def profile
+    @listings = current_user.listings
+    @listings = Listing.all
+    @requests = current_user.requests
+    @requests = Request.all
+    # how to access requests on current_user's listing?
+
   end
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,14 +1,27 @@
 class RequestsController < ApplicationController
-  before_action :set_listing, only: %i[new create]
+  before_action :set_listing, only: %i[new create show]
+  def new
+    @request = Request.new
+  end
 
   def create
     @request = Request.new(request_params)
     @request.listing = @listing
-    @request.save
-    redirect_to listing_path(@listing)
+
+    if @request.save
+      redirect_to profile_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # add edit and delete, index and show functions for current_user?
+  def index
+    @requests = Request.all
+  end
+
+  def show
+  end
 
  private
 
